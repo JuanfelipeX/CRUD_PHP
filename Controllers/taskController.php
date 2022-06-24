683ef4445dbfcfff6de68abb912931278838f3a4
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-23 19:09:53 -0500

    making the method for INSERT INTO taks

---
+++ Controllers/taskController.php
@@ -0,0 +1,22 @@
+<?php
+include("/../xampp/htdocs/CRUD_PHP/Models/connection.php");
+define('CONTROLLER_PATH', "../CRUD_PHP/Controllers/");
+define('INDEX_PATH', "../");
+
+if(isset($_POST["save-task"])){
+    $title = $_POST["title-input"];
+    $description = $_POST["description-input"];
+
+    $query = "INSERT INTO task (title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($connection, $query);
+    if ($result) {
+        header("Location:" . INDEX_PATH . "index.php");
+        $_SESSION['message'] = 'Task Saved Succesfully';
+        $_SESSION['message_type'] = 'Success';
+        
+    } else {
+        $_SESSION['error_message'] = 'check the data';
+        die("Query Failed");
+    }
+}
+?>
