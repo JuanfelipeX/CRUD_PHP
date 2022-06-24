dbf55fbcfa753bdf695bfe92157f4897842532b2
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-23 19:09:16 -0500

    refactor the code and upgrading the code

--- index.php
+++ index.php
@@ -5,28 +5,67 @@ include("../CRUD_PHP/Views/shared/header.php");
 define('CONTROLLER_PATH', "../CRUD_PHP/Controllers/");
 ?>
 
+
 <div class="container p-4">
     <div class="row">
-        <div class="col-md-4">
-            <div class="card card-body">
-                <form action="<?php echo CONTROLLER_PATH; ?>taskController.php" method="POST">
-                    <div class="mb-3">
-                        <label for="title-input" class="form-label">Task Title</label>
-                        <input type="text" class="form-control" id="title-input" name="title-input"
-                            aria-describedby="title-input" placeholder="Task Title" autofocus>
+        <div class="col">
+            <!-- INPUT SECTION -->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <?php if (isset($_SESSION['message'])) { ?>
+                        <div class="alert alert-success" role="alert">
+                            <strong><?= $_SESSION['message'] ?></strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <?php session_unset();
+                        } ?>
+                        <div class="card card-body">
+                            <form action="<?php echo CONTROLLER_PATH; ?>taskController.php" method="POST">
+                                <div class="mb-3">
+                                    <label for="title-input" class="form-label">Task Title</label>
+                                    <input type="text" class="form-control" id="title-input" name="title-input"
+                                        aria-describedby="title-input" placeholder="Task Title" autofocus>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="description-input" class="form-label">Task Description</label>
+                                    <textarea name="description-input" class="form-control" id="description-input"
+                                        name="description-input" rows="4" aria-describedby="description-input"
+                                        placeholder="Task Description"></textarea>
+                                </div>
+                                <input type="submit" class="btn btn-success btn-block" name="save-task"
+                                    value="Save Task">
+                            </form>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="description-input" class="form-label">Task Description</label>
-                        <textarea name="description-input" class="form-control" id="description-input"
-                            name="description-input" rows="4" aria-describedby="description-input"
-                            placeholder="Task Description"></textarea>
-                    </div>
-                    <input type="submit" class="btn btn-success btn-block" name="save-task" value="Save Task">
-                </form>
+                </div>
             </div>
+        </div>
+        <div class="col-sm">
+            <!-- TABLE SECTION -->
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM task";
+                    
+                    ?>
+                    <tr>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
+
 
 <?php
 include("../CRUD_PHP/Views/shared/footer.php");
