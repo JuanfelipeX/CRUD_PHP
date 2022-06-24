a434397a0fc20b926222a80b7ac1a1c97780fef1
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-23 20:50:39 -0500

    agregating font awesome cdn

--- Views/Shared/header.php
+++ Views/Shared/header.php
@@ -8,7 +8,10 @@
     <!--BOOTSTRAP 5.0.2  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--STYLES CSS  -->
     <link rel="stylesheet" href="../css/styles.css">
+
+
     <title>Document</title>
 </head>
 
